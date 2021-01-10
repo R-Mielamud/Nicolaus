2185fdd3542c9da344673ea45f9caad72b1ac578
R-Mielamud <2m.roman2@gmail.com>
2021-01-10 18:01:52 +0200

    Added ru localization

--- frontend/src/translations/ru.ts
+++ frontend/src/translations/ru.ts
@@ -1,1 +1,18 @@
-export const ru = {};
+export const ru = {
+    short_password: "Длина пароля должна быть не меньше 6 символов",
+    loading: "Загрузка...",
+    email: "Эл. почта",
+    password: "Пароль",
+    login: "Войти",
+    phone_number: "Номер телефона",
+    first_name: "Имя",
+    last_name: "Фамилия",
+    signup: "Зарегистрироваться",
+    to_home: "На главную страницу",
+    profile: "Профиль",
+    logout: "Выйти",
+    login_to_nicolaus: "Войти в Nicolaus",
+    dont_have_account: "Нет аккаунта?",
+    signup_to_nicolaus: "Зарегистрироваться в Nicolaus",
+    have_account: "Уже есть аккаунт?",
+};
