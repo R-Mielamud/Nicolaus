a87a9353c339c09a31e624ce2ae211444f1fcb1a
R-Mielamud <2m.roman2@gmail.com>
2021-01-18 22:36:29 +0200

    Little style fix

--- frontend/src/containers/ChatbotAdminPage/UsersTable/index.tsx
+++ frontend/src/containers/ChatbotAdminPage/UsersTable/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
-import { Table } from "semantic-ui-react";
+import { Tab, Table } from "semantic-ui-react";
 import Spinner from "../../../components/common/Spinner";
 import RootState from "../../../typings/rootState";
 
@@ -14,30 +14,32 @@ const UsersTable: React.FC = () => {
     }
 
     return (
-        <Table celled>
-            <Table.Header>
-                <Table.Row>
-                    <Table.HeaderCell>{t("phone")}</Table.HeaderCell>
-                    <Table.HeaderCell>{t("messenger")}</Table.HeaderCell>
-                    <Table.HeaderCell>{t("delivery_contacts")}</Table.HeaderCell>
-                    <Table.HeaderCell>{t("post_service")}</Table.HeaderCell>
-                    <Table.HeaderCell>{t("delivery_address")}</Table.HeaderCell>
-                </Table.Row>
-            </Table.Header>
-            <Table.Body>
-                {messengerUsers.map((user) => (
-                    <Table.Row key={user.messenger_id}>
-                        <Table.Cell>{user.phone}</Table.Cell>
-                        <Table.Cell>{user.messenger}</Table.Cell>
-                        <Table.Cell>
-                            {user.requisites?.delivery_phone} {user.requisites?.delivery_name}
-                        </Table.Cell>
-                        <Table.Cell>{user.requisites?.post_service}</Table.Cell>
-                        <Table.Cell>{user.requisites?.delivery_address}</Table.Cell>
+        <Tab.Pane>
+            <Table celled>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>{t("phone")}</Table.HeaderCell>
+                        <Table.HeaderCell>{t("messenger")}</Table.HeaderCell>
+                        <Table.HeaderCell>{t("delivery_contacts")}</Table.HeaderCell>
+                        <Table.HeaderCell>{t("post_service")}</Table.HeaderCell>
+                        <Table.HeaderCell>{t("delivery_address")}</Table.HeaderCell>
                     </Table.Row>
-                ))}
-            </Table.Body>
-        </Table>
+                </Table.Header>
+                <Table.Body>
+                    {messengerUsers.map((user) => (
+                        <Table.Row key={user.messenger_id}>
+                            <Table.Cell>{user.phone}</Table.Cell>
+                            <Table.Cell>{user.messenger}</Table.Cell>
+                            <Table.Cell>
+                                {user.requisites?.delivery_phone} {user.requisites?.delivery_name}
+                            </Table.Cell>
+                            <Table.Cell>{user.requisites?.post_service}</Table.Cell>
+                            <Table.Cell>{user.requisites?.delivery_address}</Table.Cell>
+                        </Table.Row>
+                    ))}
+                </Table.Body>
+            </Table>
+        </Tab.Pane>
     );
 };
 
