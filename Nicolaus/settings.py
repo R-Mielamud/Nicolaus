ecb4d482db2db5024e2050faeb186fffc98d8c01
R-Mielamud <2m.roman2@gmail.com>
2021-01-23 21:17:56 +0200

    Prepare for deploy

--- Nicolaus/settings.py
+++ Nicolaus/settings.py
@@ -83,12 +83,24 @@ WSGI_APPLICATION = 'Nicolaus.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgres_psycopg2",
+            "NAME": "nicolaus",
+            "USER": "nicolaus_user",
+            "PASSWORD": os.environ.get("POSTGRES_PWD", ""),
+            "HOST": "127.0.0.1",
+            "PORT": "5432",
+        },
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
