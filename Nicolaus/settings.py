1069c3221a39e6cdcc0489cd14db034f5c07703c
R-Mielamud <2m.roman2@gmail.com>
2021-01-23 22:00:00 +0200

    Added allowed hosts

--- Nicolaus/settings.py
+++ Nicolaus/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJ_DEBUG", "1") == "1"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["35.178.186.33", "nicolaus.com.ua", "localhost"]
 
 
 # Application definition
